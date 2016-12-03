85799e532395a2d7aac00ecfbb245a2c84c6bb8f
Joshua Westerheide <dev@jdoubleu.de>
2016-12-03 16:18:47 +0100

    Implemented functions to convert decimal numbers into numbers to a given base (and vice versa). Added helper functions to get correct representation of a non-decimal number.

--- assets/src/scripts/app.js
+++ assets/src/scripts/app.js
@@ -24,6 +24,113 @@
 	};
 
 	/**
+	 * Converts all numbers larger than 9 to a letter/symbol
+	 * @param n
+	 * @returns {string}
+	 */
+	const repr = n => {
+		return n > 9 ? String.fromCharCode(65 /* 65 is A */ + n - 10) : n;
+	};
+
+	/**
+	 * Turns a given letter/symbol/number into a number
+	 * @param {string} s
+	 * @returns {number}
+	 * @see repr
+	 */
+	const reprReverse = s => {
+		s = s.toString();
+		return s.charCodeAt(0) > 65 /* 65 is A */ ? s.charCodeAt(0)	- 65 + 10 : parseInt(s);
+	};
+
+	/**
+	 * Converts a positive! decimal number into a number to the provided base.
+	 * @param {number} number
+	 * @param base
+	 * @returns {String}
+	 */
+	const convertDecimalToBase = (number, base = 10) => {
+		let rep = "",
+			rem = number;
+
+		// Converts the given number using the base into its new representation
+		do {
+			rep = repr(rem % base) + rep;
+			rem = Math.floor(rem / base);
+		} while(rem / base > 0);
+
+		// Remove leading zeros
+		return rep.replace(/^0+(?!\.|$)/, '');
+	};
+
+	/**
+	 * Converts a number of the given base to a decimal number
+	 * @param {string} number
+	 * @param base
+	 * @returns {number}
+	 */
+	const convertBaseToDecimal = (number, base = 2) => {
+		let ret = 0;
+
+		for(let i = number.length; i > 0; i--) {
+			let pos = number.length - i;
+			ret +=	reprReverse(number.charAt(i - 1)) * Math.pow(base, pos);
+		}
+
+		return ret;
+	};
+
+	/**
+	 * Inverts a binary number according to two's complement
+	 * @param {string} bin
+	 */
+	const invertBinary = bin => {
+		// Flip all bits
+		let b = bin.split('').map(v => {
+			return v == '1' ? '0' : '1'
+		});
+
+		let i = b.length,
+			car = 1;
+		// Add +1
+		do {
+			if(car == 1)
+				if(b[i] == '1')
+					b[i] = '0';
+				else {
+					b[i] = '1';
+					car = 0;
+				}
+			i--;
+		} while(car == 1);
+
+		return b.join('');
+	};
+
+	/**
+	 * Converts a number to two's complement
+	 * @param number
+	 * @param base
+	 * @returns {String}
+	 */
+	const twosComplement = (number, base = 10) => {
+		let res = number = number.toString(),
+			sign = "+";
+
+		if(["+", "-"].indexOf(number.charAt(0)) !== -1) {
+			sign = number.charAt(0);
+			res = number.substr(1);
+		}
+
+		res = convertDecimalToBase(parseInt(res), 2);
+
+		if(sign === "-")
+			res = invertBinary(res);
+
+		return convertDecimalToBase(convertBaseToDecimal(res, 2), base);
+	};
+
+	/**
 	 * Calculator vue app
 	 * Implements the calculation logic and UI
 	 * @type {Vue}
@@ -41,9 +148,9 @@
 		computed: {
 			result: function() {
 				return checkNumber(this.value) ? {
-					bin: this.value * 2,
-					oct: this.value * 8,
-					hex: this.value * 16
+					bin: twosComplement(this.value, 2),
+					oct: twosComplement(this.value, 8),
+					hex: twosComplement(this.value, 16)
 				} : {bin:'?',oct:'?',hex:'?'};
 			}
 		}
