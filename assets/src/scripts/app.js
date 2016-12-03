414476950c845848de93c8e9d0dc6162a37b6f40
Joshua Westerheide <dev@jdoubleu.de>
2016-12-03 02:54:23 +0100

    First implementation of the calculator app using dummy convert functions

--- assets/src/scripts/app.js
+++ assets/src/scripts/app.js
@@ -4,3 +4,49 @@
  * @license MIT
  * @author Joshua Westerheide <dev@jdoubleu.de>
  */
+
+(function(w,d,V) {
+
+	/**
+	 * Regexp for a correct decimal number (as input)
+	 * @type {RegExp}
+	 */
+	const numberPattern = /^(\+|-)?(0|[1-9]+[0-9]*)$/i;
+
+	/**
+	 * Checks whether the given number is valid
+	 * @param number
+	 * @returns {boolean}
+	 * @see numberPattern
+	 */
+	const checkNumber = number => {
+		return number.toString().match(numberPattern) != null;
+	};
+
+	/**
+	 * Calculator vue app
+	 * Implements the calculation logic and UI
+	 * @type {Vue}
+	 */
+	new V({
+		el: '#app',
+		data: {
+			value: 0,
+			result: {
+				bin: 0,
+				oct: 0,
+				hex: 0
+			}
+		},
+		computed: {
+			result: function() {
+				return checkNumber(this.value) ? {
+					bin: this.value * 2,
+					oct: this.value * 8,
+					hex: this.value * 16
+				} : {bin:'?',oct:'?',hex:'?'};
+			}
+		}
+	});
+
+}(window, document, Vue));
